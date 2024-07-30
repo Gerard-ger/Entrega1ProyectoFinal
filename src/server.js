60677d250be5e9b94f88bf54aff17b84f9f70451
Gerard-ger <74406925+Gerard-ger@users.noreply.github.com>
2024-07-30 17:11:47 -0300

    correxion puerto

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const productRouter = require('./routes/products.router.js')
 const cartRouter = require('./routes/carts.router.js')
 
 const app = express()
-const PORT = 8081
+const PORT = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
